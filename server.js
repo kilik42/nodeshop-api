4b498ea8a5afef84f8b90d899e75358e6e9d366f
marvin evins <maevins@cps.edu>
2019-06-09 10:14:33 -0500

    created models

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var db = mongoose.connect('mongodb://localhost/nodeShopApi');
 
-
-
+var Product = require('./model/product');
+var WishList = require('./model/wishlist');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
