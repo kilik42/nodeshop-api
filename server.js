f513041823e95421422b12ada749380f4b21c51e
marvin evins <maevins@cps.edu>
2019-06-09 10:49:56 -0500

    added post

--- server.js
+++ server.js
@@ -11,12 +11,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 
+app.post('/product', function(request, response){
+      var product = new Product();
+      product.title = request.body.title;
+      product.price = request.body.price;
+      product.save(function(err, savedProduct){
+        if(err){
+          response.status(500).send({error: "could not save product"});
+        }else{
+          response.status(200).send(savedProduct);
 
+        }
 
-
-
-
-
+      });
+});
 
 
 app.listen(3000, function(){
